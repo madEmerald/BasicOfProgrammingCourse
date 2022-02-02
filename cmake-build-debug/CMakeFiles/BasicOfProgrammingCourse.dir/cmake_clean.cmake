9767c62f8c19fe5bb46062e4e41d9b9cc3cd850a
SolntsevNS <nikitasergsol@yandex.ru>
2022-02-02 18:54:51 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/BasicOfProgrammingCourse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BasicOfProgrammingCourse.exe"
+  "BasicOfProgrammingCourse.exe.manifest"
+  "BasicOfProgrammingCourse.pdb"
+  "CMakeFiles/BasicOfProgrammingCourse.dir/main.c.obj"
+  "libBasicOfProgrammingCourse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BasicOfProgrammingCourse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
