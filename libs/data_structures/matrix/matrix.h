db40bcfc8111957919899a24b96fd5b64c431832
SolntsevNS <nikitasergsol@yandex.ru>
2022-02-13 16:32:20 +0300

    memory usage of matrix

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -8,3 +8,11 @@ typedef struct position {
     int rowIndex;
     int colIndex;
 } position;
+
+matrix getMemMatrix(int nRows, int nCols);
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+void freeMemMatrix(matrix m);
+
+void freeMemMatrices(matrix *ms, int nMatrices);
