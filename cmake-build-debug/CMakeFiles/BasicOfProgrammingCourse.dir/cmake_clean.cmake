3d5aedfb0ce9c5dc05b3a100132e39fcfba22403
SolntsevNS <nikitasergsol@yandex.ru>
2022-02-22 23:42:31 +0300

    solve 9 problem

--- cmake-build-debug/CMakeFiles/BasicOfProgrammingCourse.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BasicOfProgrammingCourse.dir/cmake_clean.cmake
@@ -2,7 +2,7 @@ file(REMOVE_RECURSE
   "BasicOfProgrammingCourse.exe"
   "BasicOfProgrammingCourse.exe.manifest"
   "BasicOfProgrammingCourse.pdb"
-  "CMakeFiles/BasicOfProgrammingCourse.dir/main.c.obj"
+  "CMakeFiles/BasicOfProgrammingCourse.dir/9.c.obj"
   "libBasicOfProgrammingCourse.dll.a"
 )
 
