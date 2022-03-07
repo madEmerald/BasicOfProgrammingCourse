53358a71f776d258a8253f6c821bdc73076678c0
SolntsevNS <nikitasergsol@yandex.ru>
2022-03-07 13:55:34 +0300

    add string library

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -17,4 +17,8 @@ int strcmp(const char *lhs, const char *rhs);
 
 char* copy(const char *beginSource, const char *endSource, char *beginDestination);
 
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int));
+
 #endif
